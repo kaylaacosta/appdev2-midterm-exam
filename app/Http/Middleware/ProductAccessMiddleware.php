adfbb38b5331599e8d1a1f1f6e4f5481b976c692
kaylaacosta <99796243+kaylaacosta@users.noreply.github.com>
2024-04-28 20:39:08 +0800

    ProductAccessMiddleware.php

--- app/Http/Middleware/ProductAccessMiddleware.php
+++ app/Http/Middleware/ProductAccessMiddleware.php
@@ -3,18 +3,30 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ProductAccessMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
+        $validToken = env('VALID_TOKEN');
+
+        if (!$request->hasHeader('Authorization')) {
+            return response()->json(['message' => 'Token is missing'], 401);
+        }
+
+        $token = $request->header('Authorization');
+
+        if ($token !== $validToken) {
+            return response()->json(['message' => 'Token is invalid'], 401);
+        }
+
         return $next($request);
     }
 }
